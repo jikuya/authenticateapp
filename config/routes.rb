1a9c41ddb062a11c1131828c045bd0d903f445e7
jikuya <jikuya@gmail.com>
2015-08-27 16:42:50 +0900

    5. ユーザー登録画面を作成する

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'top#index'
+  get 'signup', to: 'users#new'
+  resources :users, only: [:create]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
